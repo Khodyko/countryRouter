ff50fc4f385e86c88afb82214294f7e50d1f7b0e
Igor Khodyko <Igor_Khodyko@epam.com>
2022-04-19 19:09:35 +0300

    cleanCode2

--- src/main/java/com/example/countries/CountriesApplication.java
+++ src/main/java/com/example/countries/CountriesApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @Configuration
